063b1c349ade99b2f7d07e0ce7f93566e9159d70
SWHL <liekkaskono@163.com>
2025-02-09 10:09:45 +0800

    chore: update model suffix

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ config_file = "config/smile_sliders.yaml"
 model = FLUXTextSliders(config_file)
 
 model.init_lora()
-model.load_lora_weights("outputs/flux-person-smiling/weights/slider_0.pt")
+model.load_lora_weights("outputs/flux-person-smiling/weights/slider_0.safetensors")
 
 prompt = "picture of a man"
 model.inference(prompt, num_images=2)
