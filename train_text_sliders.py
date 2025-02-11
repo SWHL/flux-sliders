635e43c3c94c534bbe79d5aece999b7f7a7c1878
SWHL <liekkaskono@163.com>
2025-02-11 08:00:37 +0800

    fix: fixed the config name

--- train_text_sliders.py
+++ train_text_sliders.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 from flux_sliders.text_sliders import FLUXTextSliders
 
-config_file = "config/smile_slider.yaml"
+config_file = "config/smile_sliders.yaml"
 model = FLUXTextSliders(config_file)
 model.train()
 model.inference("picture of a man")
